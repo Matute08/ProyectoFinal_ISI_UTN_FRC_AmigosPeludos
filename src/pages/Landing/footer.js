a0e895fd5b3671e4349cef603a5440e5b8b5b85d
unknown <matutegon97@gmail.com>
2023-08-06 19:16:04 -0300

    cambios de nombres

--- src/pages/Landing/footer.js
+++ src/pages/Landing/footer.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import { Col, Container, Row } from 'reactstrap';
-
 const Footer = () => {
     return (
         <React.Fragment>
