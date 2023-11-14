c814842e9f3d87fc5e80d7f3989b53fbd0a7ff17
unknown <matutegon97@gmail.com>
2023-11-14 12:03:10 -0300

    pago veterinaria version 5

--- src/pages/veterinarias/perfilVeterinaria/PerfilVeterinaria.js
+++ src/pages/veterinarias/perfilVeterinaria/PerfilVeterinaria.js
@@ -41,7 +41,7 @@ const PerfilVeterinaria = () => {
     const createPreference = async () => {
         try {
             const response = await axios.post(
-                "https://amigospeludos.azurewebsites.net/api/VeterinariaPago/create_preference",
+                "https://amigospeludos.azurewebsites.net/api/MercadoPagoVeterinaria/create_preference",
                 {
                     title: `Gracias por la transferencia a ${
                         veterinarias && veterinarias.nombre
